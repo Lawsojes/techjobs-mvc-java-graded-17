cb4297a9f1141be8c04521080fca06c00e9f37a9
Jesse Lawson <jesse.t.lawson@gmail.com>
2023-10-21 08:12:41 -0500

    completed tasks 1/2 working on 3

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,9 +1,15 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import org.launchcode.techjobsmvc.models.Job;
+import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
@@ -22,6 +28,18 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @PostMapping(value = "")
+    public ArrayList<Job> displaySearchResults (Model model, @RequestParam String searchTerm, @RequestParam String searchType){
+        model.addAttribute();
 
+        ArrayList<Job> jobs;
+        if(searchTerm.isEmpty() || searchType.equals("all")){
+           jobs = JobData.findAll();
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
+        return jobs;
+    }
 }
 
